49b8289c87928a20c0258f834a3733f22b5e541a
Júlia H. Zucco <juliahzucco@gmail.com>
2024-09-29 11:52:33 -0300

    DB CONNECTION

---
+++ src/Produto.java
@@ -0,0 +1,7 @@
+
+public record Produto(Long id, String nome, Integer quantidade, Double valor) {
+    
+    public Produto(String nome, Integer quantidade, Double valor){
+        this(null, nome, quantidade, valor);
+    }
+}
